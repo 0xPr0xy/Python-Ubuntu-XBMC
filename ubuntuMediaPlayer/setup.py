655d2aebf66ae8f83bc1ce3f6b9374d1f62e4513
0xPr0xy <0xPr0xy@gmail.com>
2011-10-14 17:31:07 +0200

    added ubuntuMediaPlayer

---
+++ ubuntuMediaPlayer/setup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
+
+###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
+
+import os
+import sys
+
+try:
+    import DistUtilsExtra.auto
+except ImportError:
+    print >> sys.stderr, 'To build simple-player1 you need https://launchpad.net/python-distutils-extra'
+    sys.exit(1)
+assert DistUtilsExtra.auto.__version__ >= '2.18', 'needs DistUtilsExtra.auto >= 2.18'
+
+def update_data_path(prefix, oldvalue=None):
+
+    try:
+        fin = file('simple_player1/simple_player1config.py', 'r')
+        fout = file(fin.name + '.new', 'w')
+
+        for line in fin:            
+            fields = line.split(' = ') # Separate variable from value
+            if fields[0] == '__simple_player1_data_directory__':
+                # update to prefix, store oldvalue
+                if not oldvalue:
+                    oldvalue = fields[1]
+                    line = "%s = '%s'\n" % (fields[0], prefix)
+                else: # restore oldvalue
+                    line = "%s = %s" % (fields[0], oldvalue)
+            fout.write(line)
+
+        fout.flush()
+        fout.close()
+        fin.close()
+        os.rename(fout.name, fin.name)
+    except (OSError, IOError), e:
+        print ("ERROR: Can't find simple_player1/simple_player1config.py")
+        sys.exit(1)
+    return oldvalue
+
+
+def update_desktop_file(datadir):
+
+    try:
+        fin = file('simple-player1.desktop.in', 'r')
+        fout = file(fin.name + '.new', 'w')
+
+        for line in fin:            
+            if 'Icon=' in line:
+                line = "Icon=%s\n" % (datadir + 'media/icon.png')
+            fout.write(line)
+        fout.flush()
+        fout.close()
+        fin.close()
+        os.rename(fout.name, fin.name)
+    except (OSError, IOError), e:
+        print ("ERROR: Can't find simple-player1.desktop.in")
+        sys.exit(1)
+
+
+class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
+    def run(self):
+        previous_value = update_data_path(self.prefix + '/share/simple-player1/')
+        update_desktop_file(self.prefix + '/share/simple-player1/')
+        DistUtilsExtra.auto.install_auto.run(self)
+        update_data_path(self.prefix, previous_value)
+
+
+        
+##################################################################################
+###################### YOU SHOULD MODIFY ONLY WHAT IS BELOW ######################
+##################################################################################
+
+DistUtilsExtra.auto.setup(
+    name='simple-player1',
+    version='0.1',
+    #license='GPL-3',
+    #author='Your Name',
+    #author_email='email@ubuntu.com',
+    #description='UI for managing …',
+    #long_description='Here a longer description',
+    #url='https://launchpad.net/simple-player1',
+    cmdclass={'install': InstallAndUpdateDataDirectory}
+    )
+
