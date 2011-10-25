dae913b6a3b3e2b9552cb0768a1370e364f98416
0xPr0xy <0xPr0xy@gmail.com>
2011-10-25 21:38:11 +0300

    Update genpwd.py

--- genpwd.py
+++ genpwd.py
@@ -2,7 +2,7 @@ import string
 from random import sample, choice
 
 chars = string.letters + string.digits
-length = 20
+length = 80
 a = ''.join([choice(chars) for i in range(length)]) # way 2
 
 print a
