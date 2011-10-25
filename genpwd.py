4703fbee378e832101135dfeec638e51b974f06d
0xPr0xy <0xPr0xy@gmail.com>
2011-10-25 21:36:16 +0300

    Update genpwd.py

--- genpwd.py
+++ genpwd.py
@@ -1,7 +1,8 @@
-
 import string
 from random import sample, choice
+
 chars = string.letters + string.digits
 length = 20
-''.join([choice(chars) for i in range(length)]) # way 2
+a = ''.join([choice(chars) for i in range(length)]) # way 2
 
+print a
