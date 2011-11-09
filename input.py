9491e7ac439198826e88293fb0d8f934f1b26e61
Mobile User <mobile@0xPr0xy.(none)>
2011-11-09 19:08:57 +0100

    implement changes

---
+++ input.py
@@ -0,0 +1,10 @@
+name = raw_input('What is your name?\n')
+print 'Hi, %s.' % (name)
+
+command = raw_input('specify cmd\n')
+
+if command == "ls":
+	print command
+	print "ls -las"
+else : 
+	print "else" 
--- prime.py
+++ prime.py
@@ -1,8 +1,8 @@
-for n in range(2, 100000):
+for n in range(2, 200):
 	for x in range(2, n):
 		if n % x == 0:
-			print n, 'equals', x, '*', n/x
+			#print '|no prime|', n, '=', x, '*', n/x
 			break
 	else:
          	# loop fell through without finding a factor
-		print n, 'is a prime number'
+		print '|   prime|', n
